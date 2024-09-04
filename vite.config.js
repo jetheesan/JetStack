e0ac5c2580693beca0a32be599abad32cad87d36
Jetheesan <jetheesan@gmail.com>
2024-09-04 20:56:57 +0100

    Adding Vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
